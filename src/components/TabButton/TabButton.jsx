ef60f86d749e572e5e9afff20996e897b2bc1599
Kasia <kajzerkatarzyna1@gmail.com>
2023-11-03 18:28:51 +0100

    Added Tailwind CSS styles to AsideMenu.jsx, TabButton.jsx, index.css and created CSS variables: fontFamily and colors in tailwind.config.js

--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TabButton = ({ text }) => {
     return (
-        <button>
+        <button className="hover:text-almost-white hover:bg-darker-warm-gray p-2 pr-36 text-lg">
             {text}
         </button>
     );
