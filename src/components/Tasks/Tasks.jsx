755c87288a9dc4bb2d018981e3f87bf07486ba3a
Kasia <kajzerkatarzyna1@gmail.com>
2023-11-03 17:28:00 +0100

    Added TaskInput into the Tasks.jsx, created TabContent.jsx component that contains Sections.

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Task from "../Task/Task.jsx";
+import TaskInput from "../TaskInput/TaskInput.jsx";
 
 const Tasks = () => {
     return (
-        <div>
-            <Task taskName="Learn advanced concepts"/>
-            <Task taskName="Learn the basics"/>
-        </div>
+        <>
+            <div>
+                <TaskInput/>
+            </div>
+            <div>
+                <Task taskName="Learn advanced concepts"/>
+                <Task taskName="Learn the basics"/>
+            </div>
+        </>
     );
 };
 
