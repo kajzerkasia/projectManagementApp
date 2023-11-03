1c614a2a12e17617cd7b955ba9c82453b9522bec
Kasia <kajzerkatarzyna1@gmail.com>
2023-11-03 17:11:59 +0100

    Created Tasks.jsx component.

---
+++ src/components/Tasks/Tasks.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Task from "../Task/Task.jsx";
+
+const Tasks = () => {
+    return (
+        <div>
+            <Task taskName="Learn advanced concepts"/>
+            <Task taskName="Learn the basics"/>
+        </div>
+    );
+};
+
+export default Tasks;
