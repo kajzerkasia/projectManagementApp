d86aa1a98ded9dc347b4d6b3152c220ad5381f6a
Kasia <kajzerkatarzyna1@gmail.com>
2023-11-04 16:52:11 +0100

    Moved styling from components to classes in index.css

--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TabButton = ({ text }) => {
     return (
-        <button className="hover:text-almost-white hover:bg-darker-warm-gray p-2 pr-36 text-lg">
+        <button className="tab-button">
             {text}
         </button>
     );
