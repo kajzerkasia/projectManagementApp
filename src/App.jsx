afcfb2a6e52dcb37627c020741882605bd633cb2
Kasia <kajzerkatarzyna1@gmail.com>
2023-11-04 17:08:52 +0100

    Created Button.jsx, ProjectInput.jsx and AddProject.jsx components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import AsideMenu from "./components/AsideMenu/AsideMenu.jsx";
+import AddProject from "./components/AddProject/AddProject.jsx";
 
 function App() {
-  return (
-    <>
-      <AsideMenu title="Your projects"/>
-    </>
-  );
+    return (
+        <>
+            <AsideMenu title="Your projects"/>
+            <AddProject/>
+        </>
+    );
 }
 
 export default App;
