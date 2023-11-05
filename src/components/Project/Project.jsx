6e5737c813efa8717ba1ecdffef54f4cbda09c92
Kasia <kajzerkatarzyna1@gmail.com>
2023-11-05 22:12:01 +0100

    Moved Project.jsx styles to index.css

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -4,14 +4,14 @@ import Button from "../Button/Button.jsx";
 const Project = ({projectName, date, description}) => {
 
     return (
-        <div className="flex h-screen flex-col justify-center items-start w-3/5">
+        <div className="project-container ">
             <div className="self-end pr-4">
                 <Button>Delete</Button>
             </div>
-            <h1 className="text-4xl font-bold pb-4 pl-4 ml-12 text-warm-grey">{projectName}</h1>
-            <p className="pl-4 pb-4 ml-12 text-light-warm-grey font-bold">{date}</p>
+            <h1 className="project-h1">{projectName}</h1>
+            <p className="project-p">{date}</p>
             <p className="pl-4 pb-4 ml-12">{description}</p>
-            <div className="bg-very-light-grey h-1 w-full ml-16 mt-2"></div>
+            <div className="project-border-bottom"></div>
         </div>
     );
 };
