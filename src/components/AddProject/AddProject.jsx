0a0241aa3537fcaed701b652131e0027fdb3a2e1
Kasia <kajzerkatarzyna1@gmail.com>
2023-11-05 16:59:55 +0100

    Moved AddProject.jsx Tailwind CSS styles to classes in index.css

--- src/components/AddProject/AddProject.jsx
+++ src/components/AddProject/AddProject.jsx
@@ -4,11 +4,11 @@ import Button from "../Button/Button.jsx";
 
 const AddProject = () => {
     return (
-        <div className="flex h-screen flex-col justify-center items-center w-full">
-            <div className="flex h-screen flex-col justify-center items-center w-3/5">
+        <div className="add-project-container">
+            <div className="add-project-inner-container">
                 <menu className="self-end">
-                    <Button className="w-20 mb-10 mx-0 py-2 bg-transparent ml-2 rounded text-darker-warm-grey hover:text-black text-base">Cancel</Button>
-                    <Button className="w-20 mb-10 mx-0 py-2 bg-darker-warm-grey ml-2 rounded text-almost-white hover:bg-black hover:text-white text-base">Save</Button>
+                    <Button className="add-project-cancel-button">Cancel</Button>
+                    <Button className="add-project-save-button">Save</Button>
                 </menu>
                 <main className="flex flex-col w-full">
                     <ProjectInput labelText="title" type="text"/>
@@ -21,5 +21,3 @@ const AddProject = () => {
 };
 
 export default AddProject;
-
-// @TODO: Move styles to index.css
