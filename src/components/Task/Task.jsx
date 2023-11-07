de2e67de024a6b1606f6a3608d32f2dd829642bf
Kasia <kajzerkatarzyna1@gmail.com>
2023-11-07 17:18:36 +0100

    Added tailwind CSS styles to Task.jsx

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Task = ({taskName}) => {
     return (
-        <div>
-        <p>{taskName}</p>
-        <button>Clear</button>
+        <div className="flex my-4 px-4 justify-between">
+            <p>{taskName}</p>
+            <button className="ml-4">Clear</button>
         </div>
     );
 };
