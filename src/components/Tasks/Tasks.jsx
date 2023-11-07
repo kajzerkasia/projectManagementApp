53ec9d688b224e0a5f3b67e72d99d140ddef5e51
Kasia <kajzerkatarzyna1@gmail.com>
2023-11-07 17:09:04 +0100

    Added tailwind CSS styles to Tasks.jsx

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -4,15 +4,15 @@ import TaskInput from "../TaskInput/TaskInput.jsx";
 
 const Tasks = () => {
     return (
-        <>
+        <div className="flex h-screen flex-col items-start w-3/5 pl-4 ml-12 mb-32">
             <div>
-                <TaskInput/>
+                <TaskInput labelText="Tasks"/>
             </div>
-            <div>
+            <div className="bg-the-lightest-grey w-full">
                 <Task taskName="Learn advanced concepts"/>
                 <Task taskName="Learn the basics"/>
             </div>
-        </>
+        </div>
     );
 };
 
