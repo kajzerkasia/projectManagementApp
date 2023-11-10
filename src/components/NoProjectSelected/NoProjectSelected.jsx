2bc6eac06d82be587104c54677299627681d0e07
Kasia <kajzerkatarzyna1@gmail.com>
2023-11-10 19:12:54 +0100

    Changed names of Tailwind CSS classes and imported image.

--- src/components/NoProjectSelected/NoProjectSelected.jsx
+++ src/components/NoProjectSelected/NoProjectSelected.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import homePageIcon from '../../assets/no-projects.png';
+import noProjectImage from '../../assets/no-projects.png';
 import Button from "../Button/Button.jsx";
 
 const NoProjectSelected = ({ onCreateClick }) => {
     return (
         <div className="inner-container">
-            <img src={homePageIcon} alt="No Projects Icon" className="img"/>
-            <h1 className="home-h1">No Project Selected</h1>
+            <img src={noProjectImage} alt="An empty task list" className="img"/>
+            <h1 className="no-project-selected-h1">No Project Selected</h1>
             <p className="text-light-warm-grey">Select a project or get started with a new one</p>
-            <Button className="home-button" onClick={onCreateClick}>Create new project</Button>
+            <Button className="no-project-selected-button" onClick={onCreateClick}>Create new project</Button>
         </div>
     );
 };
