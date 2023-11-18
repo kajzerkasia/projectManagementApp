7763fecb45c23c9c9d9e831aae2436bc53eb88d1
Kasia <kajzerkatarzyna1@gmail.com>
2023-11-18 21:00:04 +0100

    Added projectManagementReducer in project-management-context.jsx and moved there all logic from MainView.jsx, now using context in many places.

--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React, {useContext, useRef, useState} from 'react';
 import AsideMenu from "../AsideMenu/AsideMenu.jsx";
 import AddProject from "../AddProject/AddProject.jsx";
 import NoProjectSelected from "../NoProjectSelected/NoProjectSelected.jsx";
@@ -10,88 +10,8 @@ import {ProjectContext} from "../../store/project-management-context.jsx";
 
 const MainView = () => {
     const modal = useRef();
+    const { tabButtons, addProjectIsOpen, selectedButton, projects, onTabButtonClick, onTaskAdd, onTaskDelete} = useContext(ProjectContext);
 
-    const [mainViewState, setMainViewState] = useState({
-        tabButtons: [],
-        addProjectIsOpen: false,
-        projects: [],
-        selectedButton: null,
-    });
-
-    const handleSelect = selectedButton => {
-        setMainViewState(prevState => ({...prevState, selectedButton, addProjectIsOpen: false}));
-    };
-
-    const toggleAddProject = () => {
-        setMainViewState(prevState => ({...prevState, addProjectIsOpen: !prevState.addProjectIsOpen}));
-    };
-
-    const handleSaveClick = (newProject) => {
-        const {title, description, dueDate} = newProject;
-
-        if (title.trim() === '' || description.trim() === '' || dueDate.trim() === '') {
-            modal.current.open();
-        } else {
-            setMainViewState(prevState => ({
-                ...prevState,
-                tabButtons: [...prevState.tabButtons, title],
-                projects: [
-                    ...prevState.projects,
-                    {
-                        title,
-                        description,
-                        dueDate,
-                        tasks: [],
-                    },
-                ],
-            }));
-        }
-    };
-
-    const handleProjectDelete = projectIndex => {
-        setMainViewState(prevState => {
-            const updatedTabButtons = [...prevState.tabButtons];
-            updatedTabButtons.splice(projectIndex, 1);
-
-            const updatedProjects = [...prevState.projects];
-            updatedProjects.splice(projectIndex, 1);
-
-            return {
-                ...prevState,
-                tabButtons: updatedTabButtons,
-                projects: updatedProjects,
-                selectedButton: null,
-            };
-        });
-    };
-
-    const handleTaskAdd = (projectIndex, newTask) => {
-        setMainViewState(prevState => {
-            const updatedProjects = [...prevState.projects];
-            updatedProjects[projectIndex].tasks.push(newTask);
-            return {...prevState, projects: updatedProjects};
-        });
-    };
-
-    const handleTaskDelete = (projectIndex, taskIndex) => {
-        setMainViewState(prevState => {
-            const updatedProjects = [...prevState.projects];
-            updatedProjects[projectIndex].tasks.splice(taskIndex, 1);
-            return {...prevState, projects: updatedProjects};
-        });
-    };
-
-    const handleTabButtonClick = (index) => {
-        if (index === mainViewState.selectedButton) {
-            handleSelect(null);
-        } else {
-            handleSelect(index);
-        }
-    };
-
-    const ctxValue = {
-        onToggleAddProject: toggleAddProject,
-    };
 
     return (
         <>
@@ -100,42 +20,37 @@ const MainView = () => {
                 <p>You should complete all fields to add a new project.</p>
                 <p className="mb-4">Please make sure you provide a valid value for every input field.</p>
             </Modal>
-            <ProjectContext.Provider value={ctxValue}>
-                <div className="flex w-full h-screen">
-                    <AsideMenu menuTitle="Your projects">
-                        {mainViewState.tabButtons.map((buttonTitle, index) => (
-                            <TabButton
-                                key={index}
-                                onClick={() => handleTabButtonClick(index)}
-                                active={index === mainViewState.selectedButton}
-                            >
-                                {buttonTitle}
-                            </TabButton>
-                        ))}
-                    </AsideMenu>
-                    {mainViewState.addProjectIsOpen && <AddProject onSaveClick={handleSaveClick}/>}
-                    {!mainViewState.addProjectIsOpen && mainViewState.selectedButton !== null && mainViewState.selectedButton >= 0 && mainViewState.selectedButton < mainViewState.projects.length && (
-                        <div className="flex flex-col w-full">
-                            <Project
-                                projectIndex={mainViewState.selectedButton}
-                                projectName={mainViewState.tabButtons[mainViewState.selectedButton]}
-                                date={mainViewState.projects[mainViewState.selectedButton].dueDate}
-                                description={mainViewState.projects[mainViewState.selectedButton].description}
-                                onProjectDelete={handleProjectDelete}
-                            />
-                            <Tasks
-                                labelText="Tasks"
-                                tasks={mainViewState.projects[mainViewState.selectedButton].tasks}
-                                onTaskAdd={(newTask) => handleTaskAdd(mainViewState.selectedButton, newTask)}
-                                onTaskDelete={(taskIndex) => handleTaskDelete(mainViewState.selectedButton, taskIndex)}
-                            />
-                        </div>
-                    )}
-                    {!mainViewState.addProjectIsOpen && mainViewState.selectedButton === null && (
-                        <NoProjectSelected/>
-                    )}
-                </div>
-            </ProjectContext.Provider>
+            <div className="flex w-full h-screen">
+                <AsideMenu menuTitle="Your projects">
+                    {tabButtons.map((buttonTitle, index) => (
+                        <TabButton
+                            key={index}
+                            onClick={() => onTabButtonClick(index)}
+                            active={index === selectedButton}
+                        >
+                            {buttonTitle}
+                        </TabButton>
+                    ))}
+                </AsideMenu>
+                {addProjectIsOpen && <AddProject />}
+                {!addProjectIsOpen && selectedButton !== null && selectedButton >= 0 && selectedButton < projects.length && (
+                    <div className="flex flex-col w-full">
+                        <Project
+                            projectIndex={selectedButton}
+                            projectName={tabButtons[selectedButton]}
+                            date={projects[selectedButton].dueDate}
+                            description={projects[selectedButton].description}
+                        />
+                        <Tasks
+                            labelText="Tasks"
+                            tasks={projects[selectedButton].tasks}
+                            onTaskAdd={(newTask) => onTaskAdd(selectedButton, newTask)}
+                            onTaskDelete={(taskIndex) => onTaskDelete(selectedButton, taskIndex)}
+                        />
+                    </div>
+                )}
+                {!addProjectIsOpen && selectedButton === null && <NoProjectSelected />}
+            </div>
         </>
     );
 };
--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {ProjectContext} from "../../store/project-management-context.jsx";
 const TabButton = ({ children, active, ...props }) => {
+
     return (
         <button className={active ? 'active tab-button' : "tab-button"} {...props}>
             {children}
