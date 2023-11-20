b808c450d91d12c901ea12d8c1be90963dfe75c5
Kasia <kajzerkatarzyna1@gmail.com>
2023-11-20 12:32:15 +0100

    Moved Tailwind CSS classes from ModalWarning.jsx to index.css

--- src/components/ModalWarning/ModalWarning.jsx
+++ src/components/ModalWarning/ModalWarning.jsx
@@ -30,12 +30,12 @@ const ModalWarning = ({buttonCaption}) => {
 
 
     return (
-        <form method="dialog" className="text-center mt-4">
-            <h2 className="text-center mb-4 text-lg">Invalid Input</h2>
+        <form method="dialog" className="modal-form">
+            <h2 className="modal-h2">Invalid Input</h2>
             <p>You should complete all fields.</p>
             <p className="mb-4">Please make sure you provide a valid value for every input field.</p>
-            <div className="flex flex-col justify-center items-center">
-                <Button className="w-20 my-4 mx-0 py-2 bg-darker-warm-grey ml-2 rounded text-almost-white hover:bg-black hover:text-white text-base" onClick={onCloseModal}>{buttonCaption}</Button>
+            <div className="modal-div">
+                <Button className="modal-button" onClick={onCloseModal}>{buttonCaption}</Button>
                 <progress value={remainingTime} max={TIMER}/>
             </div>
         </form>
